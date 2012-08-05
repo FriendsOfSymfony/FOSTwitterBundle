ef362c0c7de183d523e02a8d6fd20cb542187776
Yasmany Cubela Medina <yasmanycm@gmail.com>
2012-08-05 15:03:04 -0430

    upgrading to 2.1

--- Security/Firewall/TwitterListener.php
+++ Security/Firewall/TwitterListener.php
@@ -31,22 +31,21 @@ class TwitterListener extends AbstractAuthenticationListener
 
     protected function attemptAuthentication(Request $request)
     {
-        if($request->getSession()->get("oauth_token", null) && $request->get("oauth_token", null) && 
-            $request->getSession()->get("oauth_token", null) === $request->get("oauth_token", null))
-        {
-          if (true === $this->options['use_twitter_anywhere']) {
-              if (null === $identity = $request->cookies->get('twitter_anywhere_identity')) {
-                  throw new AuthenticationException(sprintf('Identity cookie "twitter_anywhere_identity" was not sent.'));
-              }
-              if (false === $pos = strpos($identity, ':')) {
-                  throw new AuthenticationException(sprintf('The submitted identity "%s" is invalid.', $identity));
-              }
-  
-              return $this->authenticationManager->authenticate(TwitterAnywhereToken::createUnauthenticated(substr($identity, 0, $pos), substr($identity, $pos + 1)));
-          } else {
-              return $this->authenticationManager->authenticate(new TwitterUserToken());
-          }
+      if($request->getSession()->get("oauth_token", null) && $request->get("oauth_token", null) &&
+          $request->getSession()->get("oauth_token", null) === $request->get("oauth_token", null))
+      {
+        if ($this->useTwitterAnywhere) {
+            if (null === $identity = $request->cookies->get('twitter_anywhere_identity')) {
+                throw new AuthenticationException(sprintf('Identity cookie "twitter_anywhere_identity" was not sent.'));
+            }
+            if (false === $pos = strpos($identity, ':')) {
+                throw new AuthenticationException(sprintf('The submitted identity "%s" is invalid.', $identity));
+            }
+
+            return $this->authenticationManager->authenticate(TwitterAnywhereToken::createUnauthenticated(substr($identity, 0, $pos), substr($identity, $pos + 1)));
         }
-        return null;
+
+        return $this->authenticationManager->authenticate(new TwitterUserToken($request->query->get('oauth_token'), $request->query->get('oauth_verifier')));
+      }
     }
 }
